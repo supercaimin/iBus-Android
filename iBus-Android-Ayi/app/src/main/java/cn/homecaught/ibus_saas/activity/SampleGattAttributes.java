939899fb215f4c08d8ce7f529be8803bf00eed70
caixiaomin <caixiaomin@appbees.net>
2017-08-28 09:37:11 +0800

    x

--- iBus-Android-Ayi/app/src/main/java/cn/homecaught/ibus_saas/activity/SampleGattAttributes.java
+++ iBus-Android-Ayi/app/src/main/java/cn/homecaught/ibus_saas/activity/SampleGattAttributes.java
@@ -25,6 +25,9 @@ public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    public static String TARGET_BLUETOOTH_NAME = "WEIGUANGQRCODE";
+    public static String TARGET_CHARACTERISTIC_CONFIG = "0000fff1-0000-1000-8000-00805f9b34b";
+
 
     static {
         // Sample Services.
