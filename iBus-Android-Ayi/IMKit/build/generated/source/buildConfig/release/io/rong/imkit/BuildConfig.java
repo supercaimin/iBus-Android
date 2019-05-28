29f87d5c146752f6240f88cb9909429e7089f9f1
Cai Min <caimin@ibokun.com>
2019-05-28 10:25:20 +0800

    融云升级

--- iBus-Android-Ayi/IMKit/build/generated/source/buildConfig/release/io/rong/imkit/BuildConfig.java
+++ iBus-Android-Ayi/IMKit/build/generated/source/buildConfig/release/io/rong/imkit/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.rong.imkit";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2016011122;
-  public static final String VERSION_NAME = "2.6.2";
+  public static final int VERSION_CODE = 2019052416;
+  public static final String VERSION_NAME = "2.9.17 Stable";
 }
--- iBus-Android-Ayi/app/src/main/java/cn/homecaught/ibus_saas/util/ImageUtils.java
+++ iBus-Android-Ayi/app/src/main/java/cn/homecaught/ibus_saas/util/ImageUtils.java
@@ -510,7 +510,8 @@ public class ImageUtils {
 		// draw watermark into
 		cv.drawBitmap(watermark, w - ww + 5, h - wh + 5, null);// 在src的右下角画入水印
 		// save all clip
-		cv.save(Canvas.ALL_SAVE_FLAG);// 保存
+		//cv.save(Canvas.ALL_SAVE_FLAG);// 保存
+		cv.save();
 		// store
 		cv.restore();// 存储
 		return newb;
