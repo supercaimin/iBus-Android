ec7f5c453c39d14eceeeba7cc020cf820c20c3d8
Cai Min <caimin@ibokun.com>
2019-05-28 11:42:39 +0800

    解决监护人端兼容性问题

--- iBus-Android-Jhr/IMKit/build/generated/source/buildConfig/debug/io/rong/imkit/BuildConfig.java
+++ iBus-Android-Jhr/IMKit/build/generated/source/buildConfig/debug/io/rong/imkit/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.rong.imkit";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 20181023;
-  public static final String VERSION_NAME = "2.6.3";
+  public static final int VERSION_CODE = 2019052416;
+  public static final String VERSION_NAME = "2.9.17 Stable";
 }
